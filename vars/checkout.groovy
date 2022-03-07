d1915925b63d379c4251a89d16ee360503f9b725
anushareddy9678 <99065065+anushareddy9678@users.noreply.github.com>
2022-03-07 23:09:49 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,6 +1,7 @@
 def call(Map config) {
-
-checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'anupat', url: 'https://github.com/anushareddy9678/tomcat.git']]]
-
-
+checkout(
+branch:'main',
+cred:'anupat',
+giturl:'https://github.com/anushareddy9678/tomcat.git'
+)
 }
